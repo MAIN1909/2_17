8c238823468b2f9c06f682ed697dc4028affcc9b
Евгений <Main1909@ex.ua>
2018-12-04 22:04:59 +0200

    with generation all types of characters

--- src/DigitService.java
+++ src/DigitService.java
@@ -3,13 +3,29 @@ import java.util.Random;
 class DigitService {
     private static Thread[] formThreads() {
         //todo: form threads from '0 to 9', from 'a to z', from 'A to Z', '/', '_', '.'
-        Thread[] out = new Thread[10];
-        for (Integer i = 0; i < 10; i++) {
-            out[i] = new DigitFormer(i.toString());
+        Thread[] out = new Thread[65];
+        for (Integer i = 0; i < 65; i++) {
+            if (i < 10) {
+                out[i] = new DigitFormer(i.toString());
+            }
+            if (i >= 10 && i < 36) {
+                out[i] = new DigitFormer(Character.valueOf((char) (i + 55)).toString());
+            }
+            if (i >= 36 && i < 62) {
+                out[i] = new DigitFormer(Character.valueOf((char) (i + 61)).toString());
+            }
+            if (i == 62) {
+                out[i] = new DigitFormer(Character.valueOf((char) (i - 16)).toString());
+            }
+            if (i == 63) {
+                out[i] = new DigitFormer(Character.valueOf((char) (i + 35)).toString());
+            }
+            if (i == 64) {
+                out[i] = new DigitFormer(Character.valueOf((char) (i + 17)).toString());
+            }
         }
         return out;
     }
-
     private static void runThreads(Thread[] threads) {
         for (Thread t : threads) {
             t.start();
